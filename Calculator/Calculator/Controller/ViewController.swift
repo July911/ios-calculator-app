e774fc101f05a466402743ee061f221f7dc781dc
July911 <min95911@nate.com>
2021-11-19 07:47:34 +0900

    feat : CE버튼 실행 함수 구현 #3

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -3,27 +3,60 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+    @IBOutlet weak var currentNumber: UILabel!
+    @IBOutlet weak var currentOperator: UILabel!
+    
+    
     var temperaryLabel: String = ""
-    var isOperated: Bool = false
+    var needOperator: Bool = false
     
     @IBAction func touchUpNumberButton( sender: UIButton) throws {
+        needOperator = true
         
         guard let clikedNumber = sender.titleLabel?.text else {
             throw QueueError.EmptyInLinkedList
         }
-        temperaryLabel += clikedNumber
+        currentNumber.text! += clikedNumber
     }
     
     @IBAction func touchUpOperatorButton( sender: UIButton) throws {
         guard let clikedOperator = sender.titleLabel?.text else {
             throw QueueError.EmptyInLinkedList
         }
-        temperaryLabel += clikedOperator
-        isOperated = true 
+        
+        if !needOperator {
+            currentOperator.text = clikedOperator
+        }
+        
+        if needOperator {
+            cleanUpLabelText()
+        }
+        
+        temperaryLabel.append(clikedOperator)
+        currentOperator.text = clikedOperator
+    }
+    
+    func cleanUpLabelText() {
+        guard let onboardingNumber = currentNumber.text else {
+            return
+        }
+        
+        temperaryLabel += onboardingNumber
+        currentNumber.text = 0.description
+        
+    }
+    
+    @IBAction func touchUpCEbutton(_ sender: UIButton) {
+        if needOperator {
+            cleanUpLabelText()
+        }
+        
     }
 }
+
+
