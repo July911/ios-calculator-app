b0f36fd2e9ae529aaad53ca6c367396e4fb651ca
July911 <min95911@nate.com>
2021-11-22 16:20:09 +0900

    file : UML에 기반하여 MVC패턴으로 파일 분리 #5

--- Calculator/Calculator/Controller/SceneDelegate.swift
+++ Calculator/Calculator/Controller/SceneDelegate.swift
@@ -11,7 +11,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func sceneDidDisconnect(_ scene: UIScene) {
 
-
     }
 
     func sceneDidBecomeActive(_ scene: UIScene) {
--- Calculator/Calculator/Model/CalculatorItemQueue.swift
+++ Calculator/Calculator/Model/CalculatorItemQueue.swift
@@ -1,18 +1,2 @@
 import Foundation
 
-struct CalculatorItemQueue<Element> {
-    var linkedList = LinkedList<Element>(head: nil)
-    
-    func insertToQueue(_ insertValue: Element) {
-        linkedList.addNewNode(insertValue)
-    }
-
-    @discardableResult
-    func deleteFromQueue() throws -> Element {
-        guard let deletedValue = linkedList.head?.nodeValue else {
-            throw CalculatorError.emptyQueue
-        }
-        linkedList.deleteFirstNode()
-        return deletedValue
-    }
-}
