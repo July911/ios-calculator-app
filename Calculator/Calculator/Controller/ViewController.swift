5e65381fc31c695dcfb0980023301a369af56576
horeng2 <huski.a.a@gmail.com>
2021-11-25 16:27:34 +0900

    refactor: 입력중인 숫자배열 초기화 기능을 가진 resetTemporaryOperandValues() 역할 분리 #8

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -5,7 +5,92 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        temporaryOperandValues = [initialValue]
+        currentValueLabel.text = initialValue
+        isCalculated = false
+        signIsPositive = true
     }
+    
+    @IBAction private func hitOperatorButton(_ sender: UIButton) {
+        guard let inputButtonTitle = sender.titleLabel?.text else {
+            return
+        }
+        endOperandInput()
+        resetTemporaryOerandValues()
+        if isOperatorEntered {
+            valuesForCalculate.removeLast()
+            valuesForCalculate.append(inputButtonTitle)
+        } else {
+            valuesForCalculate.append(inputButtonTitle)
+            isOperatorEntered = true
+        }
+        currentOperatorLabel.text = inputButtonTitle
+    }
+    
+    func resetToInitialState() {
+        resetTemporaryOerandValues()
+        valuesForCalculate.removeAll()
+        isCalculated = false
+        currentOperatorLabel.text = ""
+    }
+    
+    func resetTemporaryOerandValues() {
+        temporaryOperandValues = [initialValue]
+        currentValueLabel.text = initialValue
+    }
+    
+    @IBAction private func hitACButton(_ sender: UIButton) {
+        resetToInitialState()
+        removeStackViewContents()
+        currentValueLabel.text = initialValue
+    }
+    
+    @IBAction private func hitCEButton(_ sender: UIButton) {
+        resetTemporaryOerandValues()
+        currentValueLabel.text = initialValue
+    }
+    
+    @IBAction private func hitCodeConversionButton(_ sender: UIButton) {
+        guard currentValueLabel.text != initialValue else {
+            return
+        }
+        guard let currentOperand = currentValueLabel.text,
+              let doubleTypeOperand = Double(currentOperand) else {
+                  return
+              }
+        signIsPositive = !signIsPositive
+        currentValueLabel.text = String(format: "%.4g", doubleTypeOperand * -1)
+    }
+
+    
+    @IBAction private func hitEqualButton(_ sender: UIButton) {
+        guard isCalculated == false,
+              valuesForCalculate != [initialValue] else {
+                  resetTemporaryOerandValues()
+              return
+        }
+        endOperandInput()
+        let calculator = ExpressionParser.self
+        let doubleTypeResult = calculator.parse(from: valuesForCalculate.joined()).result()
+        if doubleTypeResult.isNaN {
+            resetToInitialState()
+            currentValueLabel.text = "NaN"
+        } else {
+            resetToInitialState()
+            currentValueLabel.text = addCommaToValue(doubleTypeResult)
+        }
+        isCalculated = true
+    }
+    
+    private func addCommaToValue(_ value: Double) -> String {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        guard let resultWithComma = numberFormatter.string(for: value) else {
+            return "0"
+        }
+        return resultWithComma
+    }
+}
 
 
 }
