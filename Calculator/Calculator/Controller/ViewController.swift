e30649f33e0b64db0318b507bebece317fea7102
horeng2 <huski.a.a@gmail.com>
2021-11-25 16:42:41 +0900

    refactor : 코드의 명확성을 위하여 생략된 부분 표기 및 고차함수를 축약형으로 사용 #8

--- Calculator/Calculator/Controller/ViewController.swift
+++ Calculator/Calculator/Controller/ViewController.swift
@@ -4,6 +4,85 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        temporaryOperandValues = [initialValue]
+        currentValueLabel.text = initialValue
+    }
+    
+    private func addToFormulaHistory() {
+        let stackView = UIStackView()
+        stackView.spacing = 8.0
+        stackView.axis = .horizontal
+        
+        let operatorView = UILabel()
+        operatorView.text = currentOperatorLabel.text
+        operatorView.textColor = .white
+        
+        let operandView = UILabel()
+        operandView.text = currentValueLabel.text
+        operandView.textColor = .white
+        
+        stackView.addArrangedSubview(operatorView)
+        stackView.addArrangedSubview(operandView)
+        
+        formulaStackView.addArrangedSubview(stackView)
+    }
+    
+    private func removeStackViewContents() {
+        formulaStackView.arrangedSubviews.forEach { $0.removeFromSuperview() }
+    }
+    
+    @IBAction private func hitOperandButton(_ sender: UIButton) {
+        guard let inputButtonTitle = sender.titleLabel?.text else {
+            return
+        }
+        guard temporaryOperandValues.count <= 20 else {
+            return
+        }
+        
+        if temporaryOperandValues.contains(".") {
+            guard inputButtonTitle != "." else {
+                return
+            }
+        } else {
+            guard inputButtonTitle != "0" || temporaryOperandValues.first != "0" else {
+                return
+            }
+            guard inputButtonTitle != "00" || temporaryOperandValues.first != "0" else {
+                return
+            }
+        }
+        temporaryOperandValues.append(inputButtonTitle)
+        
+        if !temporaryOperandValues.contains(".") && temporaryOperandValues.first == initialValue {
+            temporaryOperandValues.removeFirst()
+        }
+        
+        let addcommaOperand: String
+        if temporaryOperandValues.contains(".") {
+            addcommaOperand = temporaryOperandValues.joined()
+        } else {
+            addcommaOperand = addCommaToValue(Double(temporaryOperandValues.joined()) ?? 0)
+        }
+        
+        if signIsPositive {
+            currentValueLabel.text = addcommaOperand
+        } else {
+            currentValueLabel.text = "-" + addcommaOperand
+        }
+        isOperatorEntered = false
+    }
+    
+    private func endOperandInput() {
+        if signIsPositive {
+            valuesForCalculate.append(temporaryOperandValues.joined())
+        } else {
+            valuesForCalculate.append("-" + temporaryOperandValues.joined())
+        }
+        
+        if valuesForCalculate != [initialValue] {
+            addToFormulaHistory()
+            formulaScrollView.scrollViewToBottom()
+        }
         
         temporaryOperandValues = [initialValue]
         currentValueLabel.text = initialValue
